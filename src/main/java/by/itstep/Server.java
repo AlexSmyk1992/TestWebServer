c96fe7e19e19fb56a9ab21489a5ebd3c835e5c85
группы стационара <st@itstep.lan>
2019-03-30 11:50:55 +0300

    initial commit

---
+++ src/main/java/by/itstep/Server.java
@@ -0,0 +1,18 @@
+package by.itstep;
+import static spark.Spark.*;
+public class Server{
+
+    public static void main(String[] args) {
+
+        get("/hello", (req, res) -> "Hello World");
+        get("/calc", (req, res) -> {
+            int a = Integer.parseInt(req.queryParams("a"));
+            int b = Integer.parseInt(req.queryParams("b"));
+            return String.valueOf(a + b);
+
+        });
+    }
+
+
+
+}
